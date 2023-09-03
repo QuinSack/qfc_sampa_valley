b96a937c69f78b5a333bc06da201e43315507fe7
PTC-TCOM02\divine.sackitey <divine.sackitey@petraonline.com>
2023-09-03 22:53:50 +0000

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/header.css'
+
+const Header = () => {
+  return (
+    <div className='headercontainer'>
+        <h4>Sign Up</h4>
+        <h4>Sign In</h4>
+    </div>
+  )
+}
+
+export default Header
