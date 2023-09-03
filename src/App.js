1dff645effc8b98e369f7ebf3012ee2c7cda5c4a
PTC-TCOM02\divine.sackitey <divine.sackitey@petraonline.com>
2023-09-03 23:21:32 +0000

    signup page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import AppRoutes from './routes/Routes';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <AppRoutes />
     </div>
   );
 }
+
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../styles/header.css'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div className='headercontainer'>
-        <h4>Sign Up</h4>
-        <h4>Sign In</h4>
+        <Link to='/signup'><h4>Sign Up</h4></Link>
+        <Link to='/login'><h4>Sign In</h4></Link>
     </div>
   )
 }
