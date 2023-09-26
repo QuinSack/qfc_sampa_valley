49696577cd4acbef887dbfe4858637f5e7bf6bfe
PTC-TCOM02\divine.sackitey <divine.sackitey@petraonline.com>
2023-09-26 21:14:26 +0000

    improved the functionality of the fellowship details page

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
         <Link to='/'><h4>Home</h4></Link>
         <Link to='/signup'><h4>Sign Up</h4></Link>
         <Link to='/login'><h4>Sign In</h4></Link>
-        <Link to='/profile'><h4>Profile</h4></Link>
+        <Link to='/fellowshipdetails'><h4>Fellowship</h4></Link>
     </div>
   )
 }
